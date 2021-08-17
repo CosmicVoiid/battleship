068308f6abd0fd103c22259f74025216cbf3543c
Saurav Sah <sah_64@yahoo.com>
2021-08-16 20:13:06 -0400

    Add Ship and Gameboard object

--- script.js
+++ script.js
@@ -2,25 +2,81 @@ class Ship {
 	constructor(shipName, size) {
 		this.shipName = shipName;
 		this.size = size;
-		this.ship = [];
+		this.shipGrid = [];
 		for (let i = 0; i < size; i++) {
-			this.ship[i] = "clear";
+			this.shipGrid[i] = "ship";
 		}
 	}
 
 	hit(position) {
-		this.ship[position] = "hit";
+		this.shipGrid[position] = "hit";
 	}
 
 	isSunk() {
-		for (let i = 0; i < this.size; i++) {}
-	}
-
-	showShip() {
-		console.log(this.ship);
+		let sunk = (val) => val === "hit";
+		return this.shipGrid.every((val) => sunk(val));
 	}
 }
 
-const ship1 = new Ship("battleship", 4);
+class Gameboard {
+	constructor(size) {
+		this.grid = [];
+		this.arrayMiss = [];
+		this.shipArray = [];
+		let row;
+		for (let i = 0; i < size; i++) {
+			row = [];
+			for (let j = 0; j < size; j++) {
+				row.push("water");
+			}
+			this.grid.push(row);
+		}
+	}
 
-module.exports = Ship;
+	placeShip(ship, rotation, row, column) {
+		if (rotation === "horizontal") {
+			for (let i = 0; i < ship.size; i++) {
+				this.grid[row][column + i] = [ship, i];
+			}
+		}
+
+		if (rotation === "vertical") {
+			for (let i = 0; i < ship.size; i++) {
+				this.grid[row + i][column] = [ship, i];
+			}
+		}
+
+		this.shipArray.push(ship);
+	}
+
+	receiveAttack(row, column) {
+		if (this.grid[row][column] === "water") {
+			this.grid[row][column] = "miss";
+			this.arrayMiss.push([row, column]);
+			return "miss";
+		} else {
+			let ship = this.grid[row][column][0];
+			ship.hit(this.grid[row][column][1]);
+			this.grid[row][column] = "hit";
+		}
+	}
+
+	allSunk() {
+		return this.shipArray.every((ship) => {
+			if (ship.isSunk()) return true;
+		});
+	}
+}
+
+class Player {
+	constructor(turnOrder, gameboard){
+		this.turnOrder = turnOrder;
+		this.gameboard = gameboard;
+	}
+
+	computerAI(){
+		this.gameboard.
+	}
+}
+
+module.exports = { Ship, Gameboard, Player };
--- script.test.js
+++ script.test.js
@@ -1,1 +1,111 @@
-const Ship = require("./script");
+const { Ship, Gameboard, Player } = require("./script");
+
+test("create a ship object", () => {
+	const ship = new Ship("ship", 4);
+	expect(ship.shipGrid).toEqual(["ship", "ship", "ship", "ship"]);
+});
+
+test("check if ship is not sunk", () => {
+	const ship = new Ship("ship", 4);
+	expect(ship.isSunk()).toBe(false);
+});
+
+test("check if ship is sunk", () => {
+	const ship = new Ship("ship", 3);
+	ship.shipGrid = ["hit", "hit", "hit"];
+	expect(ship.isSunk()).toBe(true);
+});
+
+test("check if gameboard has 10 arrays", () => {
+	const gameboard = new Gameboard(10);
+	expect(gameboard.grid.length).toEqual(10);
+});
+
+test("check if gameboard has 10 rows each with contents of 'water'", () => {
+	const gameboard = new Gameboard(10);
+	for (let i = 0; i < gameboard.size; i++) {
+		expect(gameboard.grid[i]).toEqual([
+			"water",
+			"water",
+			"water",
+			"water",
+			"water",
+			"water",
+			"water",
+			"water",
+			"water",
+			"water",
+		]);
+	}
+});
+
+test("check if gameboard can place ships horizontally", () => {
+	const gameboard = new Gameboard(10);
+	const ship = new Ship("ship", 4);
+	gameboard.placeShip(ship, "horizontal", 4, 2);
+	expect(gameboard.grid[4]).toEqual([
+		"water",
+		"water",
+		[ship, 0],
+		[ship, 1],
+		[ship, 2],
+		[ship, 3],
+		"water",
+		"water",
+		"water",
+		"water",
+	]);
+});
+
+test("check if gameboard can place ships vertically", () => {
+	const gameboard = new Gameboard(10);
+	const ship = new Ship("ship", 3);
+	gameboard.placeShip(ship, "vertical", 4, 2);
+	expect(gameboard.grid[5][2]).toEqual([ship, 1]);
+});
+
+test("check if receiveAttack function returns miss coordinates 0, 0", () => {
+	const gameboard = new Gameboard(10);
+	expect(gameboard.receiveAttack(0, 0)).toEqual("miss");
+});
+
+test("check if receiveAttack function returns hit coordinates 1, 0", () => {
+	const gameboard = new Gameboard(10);
+	const ship = new Ship("ship", 3);
+	gameboard.placeShip(ship, "horizontal", 1, 0);
+	gameboard.receiveAttack(1, 0);
+	expect(gameboard.grid[1][0]).toEqual("hit");
+});
+
+test("check if ship has registered a hit", () => {
+	const gameboard = new Gameboard(10);
+	const ship = new Ship("ship", 3);
+	gameboard.placeShip(ship, "horizontal", 1, 0);
+	gameboard.receiveAttack(1, 1);
+	expect(ship.shipGrid).toEqual(["ship", "hit", "ship"]);
+});
+
+test("check if gameboard records miss coordinates", () => {
+	const gameboard = new Gameboard(10);
+	gameboard.receiveAttack(0, 0);
+	gameboard.receiveAttack(1, 1);
+	expect(gameboard.arrayMiss).toEqual([
+		[0, 0],
+		[1, 1],
+	]);
+});
+
+test("check if gameboard allSunk works", () => {
+	const gameboard = new Gameboard(10);
+	const ship1 = new Ship("ship1", 3);
+	gameboard.placeShip(ship1, "horizontal", 0, 0);
+	gameboard.receiveAttack(0, 0);
+	gameboard.receiveAttack(0, 1);
+	gameboard.receiveAttack(0, 2);
+
+	const ship2 = new Ship("ship2", 2);
+	gameboard.placeShip(ship2, "horizontal", 5, 0);
+	gameboard.receiveAttack(5, 0);
+	gameboard.receiveAttack(5, 1);
+	expect(gameboard.allSunk()).toBe(true);
+});
